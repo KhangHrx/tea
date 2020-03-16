c29cad10d500a5b6a5304e6d910869e437232508
Pham Phuong <it.pdphuong@gmail.com>
2020-03-16 17:21:36 +0700

    selectpay

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,11 @@ Route::group(['prefix'=>'/','middleware'=>'auth'],function(){
     Route::group(['prefix'=>'cong-no'],function(){
         Route::get('danh-sach-khach-hang','LiabilityController@list')->name('liabilities.list_customer');
         Route::get('don-chua-thanh-toan/{id}','LiabilityController@unpaidList')->name('liabilities.unpaid_list');
+        Route::post('don-chua-thanh-toan/{id}','LiabilityController@postPayOrder')->name('liabilities.unpaid_list');
+
         Route::get('chi-tiet-don-chua-thanh-toan/{id}','LiabilityController@unpaidDetail')->name('liabilities.detail_unpaid');
         Route::post('chi-tiet-don-chua-thanh-toan/{id}','LiabilityController@postPayUnpaid')->name('liabilities.pay_unpaid');
+
         Route::get('don-da-thanh-toan','LiabilityController@paidList')->name('liabilities.list_paid');
         Route::get('chi-tiet-don-da-thanh-toan','LiabilityController@paidList')->name('liabilities.detail_paid');
     });
