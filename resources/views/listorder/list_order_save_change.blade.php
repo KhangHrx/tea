801e4b1ca076df2560f4e43c6f656689ad12988a
hoahtph06754 <43981901+hoahtph06754@users.noreply.github.com>
2020-03-16 17:00:09 +0700

    seed

--- resources/views/listorder/list_order_save_change.blade.php
+++ resources/views/listorder/list_order_save_change.blade.php
@@ -13,7 +13,7 @@
 								Mã đơn số : <span class="order-code">#{{ $order->id }}</span>
 							</p>
 							<div class="order-date pb-1">
-								Ngày : <span class="date-order">{{ $order->create_at }}</span>
+								Ngày : <span class="date-order">{{date('d/m/Y',strtotime($order->created_at)) }}</span>
 							</div>
 							<div class="add-new-tea mt-2">
 								<button
@@ -37,11 +37,12 @@
 												<form action="">
 													<div class="form-group">
 														<label for="tea-type">Loại chè</label>
-														<select class="form-control" id="tea-type" name="">
-															<option>Chè búp loại 1</option>
-															<option>Chè xanh loại 2</option>
-															<option>Chè xanh loại 3</option>
-															<option>Chè khô loại 3</option>
+														<select class="form-control" id="tea-type" name="" >
+																@foreach($sp as $item)
+																	<option	value="{{$item['id']}}">
+																		{{$item['name']}}
+																	</option>
+																@endforeach
 														</select>
 													</div>
 													<div class="form-group">
@@ -49,7 +50,6 @@
 															type="text"
 															id="tea-mass"
 															class="form-control"
-															placeholder="Khối lượng"
 														/>
 													</div>
 													<div class="form-group">
@@ -57,7 +57,6 @@
 															type="text"
 															id="tea-mass-rate"
 															class="form-control"
-															placeholder="Khấu trừ(%)"
 														/>
 													</div>
 													<div class="form-group">
@@ -65,7 +64,6 @@
 															type="text"
 															id="tea-mass-rate"
 															class="form-control"
-															placeholder="Khấu trừ(kg)"
 														/>
 													</div>
 													<div class="form-group">
@@ -208,7 +206,120 @@
 													</div>
 												</div>
 											</div>
+
 										
+									</td>
+									<td>
+									<div class="container d-inline">
+										<button
+											class="change-btn"
+											type="button"
+											data-toggle="modal"
+											data-target="#change-tea-type"
+										>
+											<i class="fas fa-edit text-primary"></i>
+										</button>
+										<!-- Modal -->
+										<div
+											class="modal fade text-left"
+											id="change-tea-type"
+											role="dialog"
+										>
+											<div class="modal-dialog">
+												<!-- Modal content-->
+												<div class="modal-content">
+													<div class="modal-header">
+														<h4 class="modal-title">
+															Thay đổi thông tin đơn hàng
+														</h4>
+													</div>
+													<form action="route('listorder.edit_item',[$product->orderDetail->id])" method="POST">
+														@csrf
+														@method('PUT')
+														<div class="modal-body">
+															<input type="text" value="" style="display: none;" name="id">
+															<div class="form-group">
+																<label for="item_id">Tên loại chè</label>
+																<select class="form-control" id="item_id" name="item_id" >
+																@foreach($sp as $item)
+																	<option {{ $product['product_id']==$item['id']?'selected':''}}
+																	value="{{$item['id']}}">{{$item['name']}}</option>
+																@endforeach
+																</select>
+															</div>
+
+															<div class="form-group">
+															<label for="weight">Khối lượng</label>
+																<input
+																	type="text"
+																	class="form-control"
+																	value="{{$product->weight}}" name="weight"
+																	id="weight"
+																/>
+																<div class="text-danger mt-2"></div>
+																	<div class="text-danger"></div>
+															</div>
+
+															<div class="form-group">
+															<label for="weight">Khấu trừ %</label>
+																<input
+																	type="text"
+																	class="form-control"
+																	value="{{$product->deduction_per}}" name="deduction_per"
+																/>
+																<div class="text-danger mt-2"></div>
+																	<div class="text-danger"></div>
+															</div>
+
+															<div class="form-group">
+															<label for="weight">Khấu trừ kg</label>
+																<input
+																	type="text"
+																	class="form-control"
+																	value="{{$product->deduction_kg}}" name="deduction_kg"
+																/>
+																<div class="text-danger mt-2"></div>
+																	<div class="text-danger"></div>
+															</div>
+
+															<div class="form-group">
+															<label for="unit_price">Đơn giá</label>
+																<input
+																	type="text"
+																	class="form-control"
+																	value="{{number_format($product->orderDetail->price).' '.'Đ' }}" name="unit_price"
+																/>
+																<div class="text-danger mt-2"></div>
+																	<div class="text-danger"></div>
+															</div>
+
+															<div class="form-group">
+															<label for="note">Ghi chú</label>
+																<input
+																	type="text"
+																	class="form-control"
+																	value="{{$product->note}}" name="note"
+																/>
+																<div class="text-danger mt-2"></div>
+																	<div class="text-danger"></div>
+															</div>
+
+															<div class="modal-footer">
+																<button type="submit" class="btn btn-success">Lưu thay đổi</button>
+																<button
+																	type="button"
+																	class="btn btn-secondary"
+																	data-dismiss="modal"
+																>
+																	Đóng
+																</button>
+															</div>
+														</div>
+													</form>
+												</div>
+											</div>
+										</div>
+									</div>
 									</td>
 								</tr>
 							@endforeach
